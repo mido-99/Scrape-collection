4303b988b038ea43535eebf82ec014498c96e7c5
mido <midohany910@gmail.com>
2024-02-19 00:24:19 +0200

    Direct api users approach, Remove unnecessary methods

--- udemy_users/users_scraper.py
+++ udemy_users/users_scraper.py
@@ -9,12 +9,12 @@ import requests
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
 
-BASE_SITE = "https://www.udemy.com/"
-example_cat = "https://www.udemy.com/courses/teaching-and-academics/"
-api = "https://www.udemy.com/api-2.0/discovery-units/all_courses/?p=1&page_size=48&subcategory=&instructional_level=&lang=&price=&duration=&closed_captions=&subs_filter_type=&category_id=300&source_page=category_page&locale=en_US&currency=egp&navigation_locale=en_US&skip_price=true&sos=pc&fl=cat"
-api_no_p = "https://www.udemy.com/api-2.0/discovery-units/all_courses/?page_size=16&subcategory=&instructional_level=&lang=&price=&duration=&closed_captions=&subs_filter_type=&category_id=300&source_page=category_page&locale=en_US&currency=egp&navigation_locale=en_US&skip_price=true&sos=pc&fl=cat"
 
 class UdemyUsersScraper():
+    """Extract all users page urls from a categoty on udemy.com. 
+    #TODO: add main params & main method instructions"""
+
+    BASE_SITE = "https://www.udemy.com/"
     
     def __init__(self, api):
         self.api = api
@@ -34,7 +34,7 @@ class UdemyUsersScraper():
 
         return self.re_construct_url(query, parsed_url)
 
-    def set_query(self, api, p, page_size):
+    def set_query(self, api, p: int, page_size:int):
         '''Sets the value of p (page) and page_size (itmes) in the api url'''
 
         parsed_url = urlparse(api)
@@ -44,7 +44,7 @@ class UdemyUsersScraper():
         return self.re_construct_url(query, parsed_url)
     
     def re_construct_url(self, query, parsed_url):
-        '''Reconstruct a full url out of its parts made by urlparse()'''
+        '''Reconstruct a full url out of its parts made by urllic.parse.urlparse()'''
 
         new_query = urlencode(query, doseq=True)
         new_url = urlunparse(
@@ -59,46 +59,39 @@ class UdemyUsersScraper():
         )
         return new_url
 
-    def get_api_data(self, pages):
+    def get_api_data(self, pages:int):
         '''Retrieves data by endpoint api. Can retrieve 16 result per page (default) 
         or up to 60. For efficacy and ability to devide by 16; 48 will be used.\n
         Expects number of pages in category being scraped.'''
         
-        all_courses = []    #all courses; populated on each request
-        all_requests = pages // 3   #requests to be made initially
-        remaining = pages % 3   #remaining pages
-        
+        all_users = []    #all courses; populated on each request
+        all_requests, remainder = divmod(pages, 3) #requests to be made initially, remaining pages
         #*1 The idea of // and % below
+        
         for _ in range(all_requests): 
             url = self.validate_query(self.api)
             r = requests.get(url).json()
-            course_links = [r['unit']['items'][i]['url'] for i in range(len(r['unit']['items']))]
-            all_courses.extend(course_links)
-        
-        for _ in range(remaining):  
-            url = self.set_query(self.api, (all_requests*3) + 1, 16)
+            users = [r['unit']['items'][i]['visible_instructors'][0]['url']
+                    for i in range(len(r['unit']['items']))]
+            all_users.extend(users)
+
+        for _ in range(remainder):  
+            url = self.set_query(self.api, (all_requests*3) + 1, 16)    #req_made*3 = curr_page
             r = requests.get(url).json()
-            course_links = [r['unit']['items'][i]['url'] for i in range(len(r['unit']['items']))]
-            all_courses.extend(course_links)
-            reqs +=1  
+            users = [r['unit']['items'][i]['visible_instructors'][0]['url']
+                    for i in range(len(r['unit']['items']))]
+            all_users.extend(users)
+            reqs +=1
+        return set(all_users) #A set to drop duplicates
         
-        print(all_courses[48:50], len(all_courses))
-        return all_courses
-    
-    def check_duplicates(self):
-        '''Check for duplicate users before making a request to course page.'''
-
-        pass
-    
-    def get_course_urls(self):
-        '''Get courses urls from api endpoint -rather than making hundreds of requests-.'''
-
-        pass
-    
     def get_user_data(self):
         '''Get final user data from their page'''
         
         pass
+
+example_cat = "https://www.udemy.com/courses/teaching-and-academics/"
+api = "https://www.udemy.com/api-2.0/discovery-units/all_courses/?p=1&page_size=48&subcategory=&instructional_level=&lang=&price=&duration=&closed_captions=&subs_filter_type=&category_id=300&source_page=category_page&locale=en_US&currency=egp&navigation_locale=en_US&skip_price=true&sos=pc&fl=cat"
+api_no_p = "https://www.udemy.com/api-2.0/discovery-units/all_courses/?page_size=16&subcategory=&instructional_level=&lang=&price=&duration=&closed_captions=&subs_filter_type=&category_id=300&source_page=category_page&locale=en_US&currency=egp&navigation_locale=en_US&skip_price=true&sos=pc&fl=cat"
 
 teaching = UdemyUsersScraper(api = api_no_p)
 teaching.get_api_data(7)
