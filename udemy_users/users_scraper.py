83e735bc33744dbd152abd59734a270716397eca
mido <midohany910@gmail.com>
2024-02-19 01:56:33 +0200

    Df, exprt data

--- udemy_users/users_scraper.py
+++ udemy_users/users_scraper.py
@@ -8,13 +8,23 @@ from urllib.parse import parse_qs, urlparse, urlencode, urlunparse
 import requests
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
+from pandas import DataFrame
 
 
 class UdemyUsersScraper():
     """Extract all users page urls from a categoty on udemy.com. 
     #TODO: add main params & main method instructions"""
 
-    BASE_SITE = "https://www.udemy.com/"
+    BASE_SITE = "https://www.udemy.com"
+    HEADERS = {
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
     
     def __init__(self, api):
         self.api = api
@@ -84,9 +94,40 @@ class UdemyUsersScraper():
             reqs +=1
         return set(all_users) #A set to drop duplicates
         
-    def get_user_data(self):
-        '''Get final user data from their page'''
-        
+    def get_user_data(self, users:set):
+        '''Get final user data from their pages'''
+
+        data = []
+        for user in users:
+            r = requests.get(user, headers=self.HEADERS)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            name = soup.find('h1').text
+            title = soup.find('h2', {'class': "ud-heading-md instructor-profile--instructor-title--1L6bi"})
+            tot_student, reviews = [i.text for i in soup.find_all('div', {'class': "ud-heading-xl"})]
+            social = soup.find('div', {'class': "instructor-profile--social-links--3x2ms"})
+            try:
+                links = [i['href'] for i in social.children]
+            except:
+                links = '-'
+            
+            row =  {
+                'Name': name,
+                'Title': title,
+                'Total students': tot_student,
+                'Reviews': reviews,
+                'Udemy link': self.BASE_SITE + user,
+                'Social links': links
+            }
+            data.append(row)
+        return data
+    
+    def export_data(self, data: list):
+        '''Export final data to csv file'''
+
+        df = DataFrame(data)
+        df.to_csv('data.csv', index=False, encoding='utf-8')
+    
+    def get_users_data(self, api, pages:int, export_path:str):
         pass
 
 example_cat = "https://www.udemy.com/courses/teaching-and-academics/"
