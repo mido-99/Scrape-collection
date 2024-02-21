0a016c8b6f4563e1353c4604d0d694fccd6a2815
mido <midohany910@gmail.com>
2024-02-21 19:56:19 +0200

    Accept cmd user arguments

--- habitaclia/habitaclia/spiders/rent.py
+++ habitaclia/habitaclia/spiders/rent.py
@@ -1,11 +1,16 @@
+from typing import Any, Optional
 import scrapy
 from habitaclia.items import HabitacliaItem
 
+#https://english.habitaclia.com/rent-flats-rubi.html
 
 class RentSpider(scrapy.Spider):
     name = "rent"
     allowed_domains = ["english.habitaclia.com"]
-    start_urls = ["https://english.habitaclia.com/rent-flats-rubi.html"]
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.start_urls = [kwargs.get('url')]
 
     def parse(self, response):
         links  = response.css('section.list-items:first-of-type h3 a::attr(href)').getall()
