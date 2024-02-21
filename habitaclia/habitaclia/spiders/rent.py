aec75cbdf0ddba9c13d2f00335ce411e27c1fe4b
mido <midohany910@gmail.com>
2024-02-21 18:33:58 +0200

    Fix scheme issue, Omit unwanted items

--- habitaclia/habitaclia/spiders/rent.py
+++ habitaclia/habitaclia/spiders/rent.py
@@ -8,16 +8,15 @@ class RentSpider(scrapy.Spider):
     start_urls = ["https://english.habitaclia.com/rent-flats-rubi.html"]
 
     def parse(self, response):
-        links  = response.css('h3 a::text').getall()
-        for link in links:
-            yield scrapy.Request(link, callback=self.parse_item)
-            
+        links  = response.css('section.list-items:first-of-type h3 a::attr(href)').getall()
+        yield from response.follow_all(links, callback=self.parse_item)
+
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
     def parse_item(self, response):
         yield{
-            'Title': response.css('h1').get(),
+            'Title': response.css('h1::text').get(),
             'Area': response.css('li.feature:first-of-type strong::text').get()
         }
