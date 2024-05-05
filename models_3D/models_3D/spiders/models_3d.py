de2853fe95e6d3417f91b45a0afbc1f3cb54e584
mido <midohany910@gmail.com>
2024-05-06 01:06:04 +0300

    Perform selenium actions with request.meta['data']

--- models_3D/models_3D/spiders/models_3d.py
+++ models_3D/models_3D/spiders/models_3d.py
@@ -1,7 +1,10 @@
-from typing import Iterable
 import scrapy
-from scrapy.http import Request
 from scrapy_selenium import SeleniumRequest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
+from time import sleep
 
 
 class Models3dSpider(scrapy.Spider):
@@ -14,7 +17,12 @@ class Models3dSpider(scrapy.Spider):
 
     def parse(self, response):
         base_url = "https://www.printables.com"
-        
+        driver, wait = self.setup_driver(response)
+        self.click_homePage_button(driver, wait)
+        for _ in range(10):
+            ActionChains(driver).scroll_by_amount(0, 10000).perform()
+            sleep(0.5)
+
         for product in response.css('print-card'):
             name = product.css('a.link.clamp-two-lines::text').get()
             likes = product.css('span.count.cursor-pointer::text').get()
@@ -28,3 +36,16 @@ class Models3dSpider(scrapy.Spider):
                 "Downloads": downloads,
                 "Link": link
             }
+    
+    def setup_driver(self, response):
+        
+        driver = response.request.meta['driver']
+        wait = WebDriverWait(driver, 15)
+        return driver, wait
+        
+    def click_homePage_button(self, driver, wait):
+        
+        button = wait.until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler")))
+        button.click()
+
+    
